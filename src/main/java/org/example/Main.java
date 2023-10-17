a897942cd3568d6924e1bab2bd6ab0c41cb353e1
Dream_Autumn <lihaoxin@bupt.edu.cn>
2023-10-17 15:32:31 +0800

    create time

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * ClassName: ${NAME}
+ * Package: org.example
+ * Description:
+ *
+ * @Author 幻秋
+ * @Create 2023/10/17  15:30
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
